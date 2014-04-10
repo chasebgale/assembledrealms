d10cc0ae048c59fc7aea1d98786914e72cd2f4e0
Chase B. Gale <chase.b.gale@gmail.com>
2014-04-10 11:55:12 -0400

    Directory organization

--- www/gitlab.php
+++ www/gitlab.php
@@ -4,16 +4,21 @@ require_once("models/config.php");
 if (!securePage($_SERVER['PHP_SELF'])){die();}
 
 if(!isUserLoggedIn()) { 
-  echo "<h1>You must be logged in to access our build tools!</h1>";
+  echo "No auth.";
   die(); 
 }
 
 //$username = sanitize(trim($_POST["username"]));
 //$password = trim($_POST["password"]);
-  
-if ($loggedInUser->gitlab_password == "") {
 
-    $curl = curl_init();
+$new_project = sanitize(trim($_POST["name"]));
+
+$curl = curl_init();
+$admin_token = "iHrbUiraXaAaiDiNgMAV";
+
+// FIRST: Create user in gitlab if need be
+
+if ($loggedInUser->gitlab_password == "") {
         
     $generated_pass = randomPassword();
         
@@ -23,7 +28,7 @@ if ($loggedInUser->gitlab_password == "") {
     $fields .= "name=" . $loggedInUser->displayname;
        
     curl_setopt_array($curl, array(
-        CURLOPT_HTTPHEADER => array('PRIVATE-TOKEN: iHrbUiraXaAaiDiNgMAV'),
+        CURLOPT_HTTPHEADER => array('PRIVATE-TOKEN: ' . $admin_token),
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_POST => 1,
         CURLOPT_URL => 'http://source-01.assembledrealms.com/api/v3/users',
@@ -31,16 +36,31 @@ if ($loggedInUser->gitlab_password == "") {
     ));
         
     $resp = curl_exec($curl);
-    curl_close($curl);
+    
         
     // {"message":"400 (Bad request) \"name\" not given"}
     // TODO: JSON PARSE RESPONSE AND SEE IF WE HAVE A 'message' LIKE ABOVE, THIS INDICATES ERROR
     
+    $loggedInUser->updateGitlab($generated_pass);
+    
 }
 
 
-    
-   
+// SECOND: Create new project
+$fields =  "user_id=" . $loggedInUser->username . "&";
+$fields .= "name=" . $new_project;
 
+curl_setopt_array($curl, array(
+    CURLOPT_HTTPHEADER => array('PRIVATE-TOKEN: ' . $admin_token),
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_POST => 1,
+    CURLOPT_URL => 'http://source-01.assembledrealms.com/api/v3/projects/user/' . $loggedInUser->username,
+    CURLOPT_POSTFIELDS => $fields
+));
+
+$resp = curl_exec($curl);
+
+
+curl_close($curl);
 
 ?>
