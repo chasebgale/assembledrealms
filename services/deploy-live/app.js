915ac759a4fd4a3f2d3b979d420827b642f86eb6
Chase B. Gale <chase.b.gale@gmail.com>
2014-06-10 20:08:18 -0400

    Starting to migrate to nodegit over gitlab, woo!!

---
+++ services/deploy-live/app.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , bodyParser = require('body-parser')
+  , project = require('./routes/project');
+
+var app = express();
+
+// CORS
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://www.assembledrealms.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+// Configuration
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(bodyParser());
+app.use(allowCrossDomain);
+app.use(express.static(__dirname + '/public'));
+
+// Routes
+
+app.get('/', function(req, res){
+  res.send("assembledrealms api 0.1");  
+});
+
+app.get('/api/project/create/:id', project.create);
+
+app.listen(80, function(){
+  console.log("Express server listening on port 80.");
+});
