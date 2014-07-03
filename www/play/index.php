b98ad6a9edc0a29c1d4f0a3e55e949ca4f3968ab
Chase B. Gale <chase.b.gale@gmail.com>
2014-07-02 22:17:44 -0400

    More revamping and productioning of play

--- www/play/index.php
+++ www/play/index.php
@@ -2,43 +2,52 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'] . "models/config.php");
 if (!securePage($_SERVER['PHP_SELF'])){die();}
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method == 'POST') {
+    
+    $page = 0;
+    $count = 20;
+    
+    if (isset($_POST['page'])) {
+        if (is_numeric($_POST['page'])) {
+            $page = $_POST['page'];
+        }
+    }
+    
+    if (isset($_POST['count'])) {
+        if (is_numeric($_POST['count'])) {
+            $count = $_POST['count'];
+        }
+    }
+    
+    global $mysqli,$db_table_prefix;
+    
+    // COUNT
+    $stmt = $mysqli->prepare("SELECT * FROM realms");
+    $result = $stmt->execute();
+    $stmt->store_result();
+    $totalrows = $stmt->num_rows;
+    $stmt->close();
+    
+    $totalpages = ceil($totalrows / $count);
+    $start = $page * $count;
+    
+    $stmt = $mysqli->prepare("SELECT * FROM realms LIMIT " . $start . ", " . $count);
+    $result = $stmt->execute();
+    $stmt->bind_result($id, $user_id, $title, $description, $status, $players_online, $funds, $screenshots, $loves);
+            
+    while ($stmt->fetch()){
+        $row[] = array('id' => $id, 'user_id' => $user_id, 'title' => $title, 'description' => $description, 'status' => $status, 'players_online' => $players_online, 'funds' => $funds, 'screenshots' => $screenshots, 'loves' => $loves);
+    }
+    $stmt->close();
+    
+    echo json_encode($row);
+    die();
+}
+
 require_once($_SERVER['DOCUMENT_ROOT'] . "models/header.php");
-
-$page = 0;
-$count = 20;
-
-if (isset($_GET['page'])) {
-    if (is_numeric($_GET['page'])) {
-        $page = $_GET['page'];
-    }
-}
-
-if (isset($_GET['count'])) {
-    if (is_numeric($_GET['count'])) {
-        $count = $_GET['count'];
-    }
-}
-
-global $mysqli,$db_table_prefix;
-
-// COUNT
-$stmt = $mysqli->prepare("SELECT * FROM realms");
-$result = $stmt->execute();
-$stmt->store_result();
-$totalrows = $stmt->num_rows;
-$stmt->close();
-
-$totalpages = ceil($totalrows / $count);
-$start = $page * $count;
-
-$stmt = $mysqli->prepare("SELECT * FROM realms LIMIT " . $start . ", " . $count);
-$result = $stmt->execute();
-$stmt->bind_result($id, $user_id, $title, $description, $status, $players_online, $funds, $screenshots);
-        
-while ($stmt->fetch()){
-    $row[] = array('id' => $id, 'user_id' => $user_id, 'title' => $title, 'description' => $description, 'status' => $status, 'players_online' => $players_online, 'funds' => $funds, 'screenshots' => $screenshots);
-}
-$stmt->close();
 
 ?>
 
@@ -69,93 +78,89 @@ $stmt->close();
         </div>
     </div>
     
-    <?php
-    
-        $alternate = false;
-    
-        foreach($row as $realm) {
-            if ($alternate) {
-                $output = '<div class="playListRealm" style="border: #eee solid 1px;">';
-            } else {
-                $output = '<div class="playListRealm">';
-            }
-            
-            $alternate = !$alternate;
-            
-            // Title
-            $output .= '<a href="realm/' . $realm['id'] . '"><h3>' . $realm['title'] . '<div class="pull-right"><small>';
-            
-            // Online / Offline + users
-            if ($realm['status'] == 1) {
-                $output .= '<span class="label label-success"><i class="fa fa-power-off"></i> Online</span>';
-                $output .= '<span class="label label-default" style="margin-left: 6px;"><i class="fa fa-child"></i> ' . $realm['players_online'] . '</span>';
-            } else {
-                $output .= '<span class="label label-default"><i class="fa fa-power-off"></i> Offline</span>';
-            }
-            
-            // Likes
-            $output .= '<span class="label label-default" style="margin-left: 6px;"><i class="fa fa-heart"></i> 12</span>';
-            $output .= '</small></div></h3></a>';
-            
-            if ($realm['screenshots']) {
-                $output .= '<div class="row wrapper-parent">';
-                
-                // Screenshots are in the format {id}-{#}-thumb.jpg and {id}-{#}.jpg, e.g. 42-1.jpg and 42-1-thumb.jpg
-                for ($i = 0; $i < $realm['screenshots']; $i++) {
-                    $output .= '<div class="col-md-2">';
-                    $output .= '<a href="img/' . $realm['id'] . '-' . $i . '.jpg" data-toggle="lightbox" data-title="' . $realm['title'] . ' <small> screenshot #' . ($i + 1) . ' </small>" data-parent=".wrapper-parent" data-gallery="gallery-' . $realm['id'] . '" class="thumbnail">';
-                    $output .= '<img src="img/' . $realm['id'] . '-' . $i . '-thumb' . '.jpg"></a></div>';
-                }
-                
-                $output .= '</div>';
-            }
-            
-            if ($realm['description']) {
-                $output .= '<p class="text-justify">' . $realm['description'] . '</p>';
-            }
-            
-            $output .= '</div>';
-            
-            /*
-            
-            $output .= '<td>' . $realm['id'] . '</td>';
-            
-            $output .= '<td>' . $realm['published'] . '</td>';
-            $output .= '<td>' . $realm['likes'] . '</td>';
-            
-            $output .= '</tr>';
-            */
-            
-            echo $output;
-        }
-    ?>
-    
-    <ul class="pagination pagination-sm">
-        <li><a href="#">&laquo;</a></li>
-        <?php
-            for ($i = 0; $i < $totalpages; $i++) {
-                if ($i == $page) {
-                    echo '<li class="active"><a href="#">' . ($i + 1) . ' <span class="sr-only">(current)</span></a></li>';
-                } else {
-                    echo '<li><a href="#">' . ($i + 1) . '</a></li>';
-                }
-            }
-        ?>
-        <li><a href="#">&raquo;</a></li>
-    </ul>
+    <section id="realmList"></section>
         
 </div>
+
+<script id="realms_template" type="text/template">
+    
+    <div id="results">
+    
+    <% _.each( realms, function( realm ){ %>
+    
+        <div class="playListRealm">
+            
+            <!-- Title -->
+            <a href="realm/<%- realm.id %>">
+            <h3> <%- realm.title %>
+            <div class="pull-right">
+            <small>
+            
+            <!--  Online / Offline + users  -->
+            <% if (realm.status == 1) { %>
+                <span class="label label-success"><i class="fa fa-power-off"></i> Online</span>
+                <span class="label label-default" style="margin-left: 6px;"><i class="fa fa-child"></i> <%- realm.players_online %></span>
+            <% } else { %>
+                <span class="label label-default"><i class="fa fa-power-off"></i> Offline</span>
+            <% } %>
+            
+            <!--  Loves -->
+                <span class="label label-default" style="margin-left: 6px;"><i class="fa fa-heart"></i> <%- realm.loves %></span>
+                
+            </small>
+            </div>
+            </h3>
+            </a>
+            
+        <% if (realm.screenshots) { %>
+            <div class="row wrapper-parent">
+            
+            <!-- Screenshots are in the format {id}-{#}-thumb.jpg and {id}-{#}.jpg, e.g. 42-1.jpg and 42-1-thumb.jpg -->
+            <% for (var i = 0; i < realm.screenshots; i++) { %>
+                <div class="col-md-2">
+                    <a href="img/<%- realm.id + '-' + i + '.jpg' %>"
+                       data-toggle="lightbox"
+                       data-title="<%- realm.title + '<small> screenshot #' + (i + 1) + ' </small>' %>"
+                       data-parent=".wrapper-parent"
+                       data-gallery="<%- 'gallery-' + realm.id %>" class="thumbnail">
+                        <img src="img/<%- realm.id + '-' + i + '-thumb.jpg' %>">
+                    </a>
+                </div>
+            <% } %>
+            
+            </div>
+        <% } %>
+        
+        <% if (realm.description) { %>
+            <p class="text-justify"><%- realm.description %></p>
+        <% } %>
+            
+        </div>
+        
+    <% }); %>
+    
+    </div>
+    
+    <% if (initial) { %>
+        <ul id="realmPagination" class="pagination pagination-sm">
+            <li><a id="prevPage" href="#">&laquo;</a></li>
+        <% for (var i = 0; i < pages; i++) { %>
+            <% if (i == page) { %>
+                <li class="active pageNumber"><a href="#" data-page="<%- i %>"><%- (i + 1) %> <span class="sr-only">(current)</span></a></li>
+            <% } else { %>
+                <li><a class="pageNumber" href="#" data-page="<%- i %>"><%- (i + 1) %></a></li>
+            <% } %>
+        <% } %>
+            <li><a id="nextPage" href="#">&raquo;</a></li>
+        </ul>
+    <% } %>
+    
+</script>
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "models/footer.php"); ?>
 
 <script src="js/ekko-lightbox.min.js" type="text/javascript" charset="utf-8"></script>
-
-<script type="text/javascript">
-    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-    }); 
-</script>
+<script src="js/index.js" type="text/javascript" charset="utf-8"></script>
 
 </body>
 </html>
---
+++ www/play/js/index.js
@@ -0,0 +1,71 @@
+// TODO: Grab from URL parameters so users can save favorite searches
+var __page = 0;
+var __pageCount = 0;
+var __resultsPerPage = 10;
+
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+function searchRealms(initial) {
+    
+    initial = typeof initial !== 'undefined' ? initial : true;
+    
+    var templateFn = _.template($('#realms_template').html());        
+    var parameters = {};
+    
+    // TODO: Load parameters from search control values
+    parameters.directive = "realms";
+
+    $.post("index.php", parameters, function (data) {
+        if (data !== "null") {
+            data = JSON.parse( data );
+            
+            if (initial) {
+                __pageCount = Math.ceil(data.length / __resultsPerPage);
+            }
+            
+            $("#realmList").html(templateFn({ 'realms':  data, 'initial': initial, 'page': __page, 'pages': __pageCount }));
+            
+            if (initial) {
+                $("#results").fadeIn();
+            }
+        }
+    });
+}
+
+$(document).ready(function () {
+    searchRealms();
+    
+    $('#prevPage').on('click', function (e) {
+        e.preventDefault();
+        
+        __page--;
+        
+        if ((__page >= 0) && (__page <= __pageCount)) {
+            searchRealms(false);
+        }
+    });
+    
+    $('#nextPage').on('click', function (e) {
+        e.preventDefault();
+        
+        __page++;
+        
+        if ((__page >= 0) && (__page <= __pageCount)) {
+            searchRealms(false);
+        }
+    });
+    
+    $('.pageNumber').on('click', function (e) {
+        e.preventDefault();
+        
+        __page == $(this).attr('data-page');
+        
+        if ((__page >= 0) && (__page <= __pageCount)) {
+            searchRealms(false);
+        }
+    });
+    
+});
