abb18b67a25eceb6198e70b3b9916f019a1e54f8
Chase B. Gale <chase.b.gale@gmail.com>
2014-07-08 16:02:04 -0400

    realm.php work

--- www/play/realm.php
+++ www/play/realm.php
@@ -29,13 +29,35 @@ if (is_numeric($_SERVER['QUERY_STRING'])) {
 ?>
 
 <div id="content">
-    <div id="realmBar" class="navbar-default" style="margin-bottom: 40px;">
+    
+    
+    <div id="realmBar" class="center-block clearfix" style="margin-bottom: 40px;">
+        
+        <h2 class="text-center"><?=$realm['title']?></h2>
+
+        <div class="media pull-left">
+            <a class="pull-left" href="#">
+                <img width="50" height="50" class="media-object" src="/img/profiles/<?=$realm['user_id'] . ".jpg?" . time() ?>">
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Display Name<br />
+                <small>published this realm on 12.03.14</small></h4>
+            </div>
+        </div>
+        
+        <div class="pull-right">
+            <button id="btnLove" type="button" class="btn btn-default navbar-btn" data-toggle="button"><i class="fa fa-heart-o"></i> Love</button>
+            <button type="button" class="btn btn-default navbar-btn"><i class="fa fa-comments-o"></i> Comment</button>
+        </div>
+            
+        <!--
         <div class="container">
             <button type="button" class="btn btn-default navbar-btn" data-toggle="button"><i class="fa fa-heart-o"></i> Love</button>
             <button type="button" class="btn btn-default navbar-btn active" data-toggle="button"><i class="fa fa-heart"></i>  Loved!</button>
-            <a href="#">Loves</a>
-            <a href="#">Loves</a>
         </div>
+    
+        Array ( [id] => 43 [user_id] => 1 [title] => Just Another REALM [description] => This is a short description, I don't really feel like writing one. [status] => 1 [players] => 8 [funds] => 0.00 [screenshots] => 2 [loves] => 14 [url] => debug-01.assembledrealms.com ) 1
+        -->
     </div>
     
     <?php
@@ -49,6 +71,22 @@ if (is_numeric($_SERVER['QUERY_STRING'])) {
 </div>
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "models/footer.php"); ?>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+       $('#btnLove').on('click', function (e) {
+            e.preventDefault();
+            
+            var button = $(this);
+            
+            if (!button.hasClass('active')) {
+                button.html('<i class="fa fa-heart"></i>  Loved!');
+                button.prop("disabled",true);
+            }
+            
+       });
+    });
+</script>
 
 </body>
 </html>
