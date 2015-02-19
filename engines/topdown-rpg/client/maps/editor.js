f40e577db9050425de3325024b60e51f2ad3f906
Chase B. Gale <chase.b.gale@gmail.com>
2015-02-19 17:31:34 -0500

    Adding and drawing tile refinements

--- engines/topdown-rpg/client/maps/editor.js
+++ engines/topdown-rpg/client/maps/editor.js
@@ -4,21 +4,23 @@ var Map = {
 	renderer: null,
     invalidate: false,
 	width: 896,
-	height: 504,
+	height: 512, // Slightly larger than 16:9, so we have an even height in tiles
 	half_width: 448,
-	half_height: 252,
+	half_height: 256,
 	TILE_WIDTH: 32,
 	TILE_WIDTH_HALF: 16,
 	TILE_HEIGHT: 32,
 	TILE_HEIGHT_HALF: 16,
 	
-	// Center global coordinates of viewport in relation to the map: 
-	coordinates: {x: 0, y: 0},
+	// Top left tile coordinates
+	coordinates: {row: 0, col: 0},
     
     offset: {x: 0, y: 0},
     tile_count: 0,
     tile_index: 0,
     mouse_down: false,
+    
+    debug: true,
 	
 	init: function (element, map) {
         
@@ -66,6 +68,12 @@ var Map = {
             if (Map.mouse_down) {
                 Map.setTile({x: event.layerX, y: event.layerY}, Map.tile_index);
             }
+            
+            var x = event.layerX - (event.layerX % Map.TILE_WIDTH);
+            var y = event.layerY - (event.layerY % Map.TILE_HEIGHT);
+            
+            Map.mouse_sprite.x = x;
+            Map.mouse_sprite.y = y;
         }
         
 
@@ -119,8 +127,10 @@ var Map = {
 
             Map.layer_terrain = new PIXI.Sprite(Map.texture);
             Map.layer_terrain.cacheAsBitmap = true;
-            
             Map.stage.addChild(Map.layer_terrain);
+            
+            Map.mouse_sprite = new PIXI.Sprite(PIXI.Texture.fromFrame('tile_0'));
+            Map.stage.addChild(Map.mouse_sprite);
             
             requestAnimationFrame(Map.render);
         };
@@ -175,8 +185,8 @@ var Map = {
 	},
 	
 	setTile: function (screen_coordinates, tile_index) {
-		var col = Math.floor((Map.coordinates.x - Map.offset.x - Map.half_width + screen_coordinates.x) / Map.TILE_WIDTH);
-		var row = Math.floor((Map.coordinates.y - Map.offset.y - Map.half_height + screen_coordinates.y) / Map.TILE_HEIGHT);
+		var col = Map.coordinates.col + Math.floor(screen_coordinates.x / Map.TILE_WIDTH);
+		var row = Map.coordinates.row + Math.floor(screen_coordinates.y / Map.TILE_HEIGHT);
         
         if (Map.terrain[row] === undefined) {
             Map.terrain[row] = {};
@@ -185,6 +195,10 @@ var Map = {
 		
 		Map.terrain[row][col] = tile_index;
 		Map.invalidate = true;
+        
+        if (Map.debug) {
+            console.log("setTile()'d at row: " + row + ", col: " + col);
+        }
 	},
 	
 	draw: function (full) {
@@ -195,27 +209,24 @@ var Map = {
     
 		var sprite;
 		
-		var start_col = Math.floor((Map.coordinates.x - Map.half_width) / Map.TILE_WIDTH);
-		var start_row = Math.floor((Map.coordinates.y - Map.half_height) / Map.TILE_HEIGHT);
-		
-		var offset_x = (Map.coordinates.x - Map.half_width) % Map.TILE_WIDTH;
-		var offset_y = (Map.coordinates.y - Map.half_height) % Map.TILE_HEIGHT;
-		
-		for (var row = start_row; row < Map.VIEWPORT_HEIGHT_TILES; row++) {
-			for (var col = start_col; col < Map.VIEWPORT_WIDTH_TILES; col++) {
+		for (var row = Map.coordinates.row; row < Map.VIEWPORT_HEIGHT_TILES; row++) {
+			for (var col = Map.coordinates.col; col < Map.VIEWPORT_WIDTH_TILES; col++) {
 				if (Map.terrain[row] === undefined) continue;
 				if (Map.terrain[row][col] === undefined) continue;
 				
 				index = Map.terrain[row][col];
 				sprite = new PIXI.Sprite(PIXI.Texture.fromFrame('tile_' + index));
 				
-				sprite.position.x = offset_x + ((col - start_col) * Map.TILE_WIDTH);
-				sprite.position.y = offset_y + ((row - start_row) * Map.TILE_HEIGHT);
-				Map.buffer.addChild(sprite);
+				//sprite.position.x = offset_x + ((col - start_col) * Map.TILE_WIDTH);
+				//sprite.position.y = offset_y + ((row - start_row) * Map.TILE_HEIGHT);
+				sprite.position.x = ((col - Map.coordinates.col) * Map.TILE_WIDTH);
+				sprite.position.y = ((row - Map.coordinates.row) * Map.TILE_HEIGHT);
+                
+                Map.buffer.addChild(sprite);
 			}
 		}
         
-        Map.offset = {x: offset_x, y: offset_y};
+        //Map.offset = {x: offset_x, y: offset_y};
 		
         
 		if (full) {
@@ -274,10 +285,13 @@ var Map = {
                                 '</select>' +
                             '</div>' +
                             '<div id="modalTilesBody" class="modal-body" style="overflow-x: scroll;">' +
+                                // TILE HOVER IDENTIFIER:
+                                '<div id="tilesHoverIdentifier" style="width: 32px; height: 32px; outline: 2px solid magenta; position: relative; z-index: 9999; display: none; pointer-events: none;"></div>' + 
                             '</div>' +
                         '</div>' +
                     '</div>' +
                 '</div>';
+                
         
         // ADD MARKUP TO DOM:
         document.getElementById('mapToolbar').innerHTML = html;
@@ -294,9 +308,29 @@ var Map = {
             var rows_to_add = Math.floor(point.y / 32);
             
             Map.tile_index = (rows_to_add * row_tile_width) + Math.floor(point.x / 32);
+            Map.mouse_sprite.setTexture(PIXI.Texture.fromFrame('tile_' + Map.tile_index));
+            
             $("#modalTiles").modal("hide");
             
         });
+        
+        $('#modalTilesBody').on('mousemove', 'img', function (e) {
+            $("#tilesHoverIdentifier").show();
+            
+            var offset = $(this).offset();
+            var point = {x: e.pageX - offset.left, y: e.pageY - offset.top};
+            
+            var y_offset = point.y % 32;
+            var x_offset = point.x % 32;
+            
+            $("#tilesHoverIdentifier").css('top', (point.y - y_offset + 32) + 'px');
+            $("#tilesHoverIdentifier").css('left', (point.x- x_offset) + 'px');
+            
+        });
+        
+        $('#modalTiles').on('hidden.bs.modal', function (e) {
+            $("#tilesHoverIdentifier").hide();
+        })
         
         $("#addButton").on("click", function () {
        
