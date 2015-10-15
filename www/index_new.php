42664cb76f8f68ab57b968a0ed8521af7ed692a2
Chase B. Gale <chase.b.gale@gmail.com>
2015-10-14 20:42:52 -0400

    clean up, front page tweak

--- www/index_new.php
+++ www/index_new.php
@@ -10,7 +10,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "models/header.php");
 
 <div id="intro" style="width: 100%; height: 100vh; background-size: cover; background-image:url('/img/front_page_background.png'); color: white;">
   <h1 class="text-center" style="padding-top: 100px;"><strong>BUILD AN AWESOME MMO IN YOUR BROWSER</strong></h1>
-  <h4 class="text-center">quick-start from an existing game &#9642; developer/community tools &#9642; client/server code in one language: javascript</h4>
+  <h3 class="text-center">QUICK-START FROM AN EXISTING GAME &#9642; DEVELOPER/COMMUNITY TOOLS</h3> <h3 class="text-center"><strong>CLIENT/SERVER CODE IN ONE LANGUAGE: JAVASCRIPT</strong></h3>
 </div>
 
 <div id='content' class="container">
