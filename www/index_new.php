dbdc99aee03396965b6f9f44a4f5d6cf4666e3bd
Chase B. Gale <chase.b.gale@gmail.com>
2015-12-22 16:53:30 -0500

    Footer almost done

--- www/index_new.php
+++ www/index_new.php
@@ -1,11 +1,7 @@
 <?php
-
-
-
 require_once($_SERVER['DOCUMENT_ROOT'] . "models/config.php");
 if (!securePage($_SERVER['PHP_SELF'])){die();}
 require_once($_SERVER['DOCUMENT_ROOT'] . "models/header.php");
-
 ?>
 
 <div id="intro" style="width: 100%; height: 100vh; background-size: cover; background-image:url('/img/front_page_background.png'); color: white;">
@@ -13,16 +9,8 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "models/header.php");
   <h3 class="text-center">QUICK-START FROM AN EXISTING GAME &#9642; DEVELOPER/COMMUNITY TOOLS</h3> <h3 class="text-center"><strong>CLIENT/SERVER CODE IN ONE LANGUAGE: JAVASCRIPT</strong></h3>
 </div>
 
-<div id='content' class="container">
-  
-  
-  
-</div>
-
 <?php
-
 require_once($_SERVER['DOCUMENT_ROOT'] . "models/footer.php");
-
 ?>
 
 </body>
--- www/models/footer.php
+++ www/models/footer.php
@@ -1,39 +1,55 @@
-<div id='footer' class="container">
-    <script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
-    <script src='//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>
-    <!--
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- assembledrealms large 
-    <ins id="footer_ad" class="adsbygoogle"
-         style="display:block;width:970px;height:90px; margin: 0 auto; margin-top: 100px;"
-         data-ad-client="ca-pub-4898800745718155"
-         data-ad-slot="4211616849"></ins> 
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    
-    <script>
-        $(function() {
-            console.log('Document ready!');
-            setTimeout(
-              function() 
-              { 
-                console.log('1 second later and...');
-                if ($('#footer_ad').is(':hidden')) {
-                    $('#footer_msg').fadeIn();
-                    console.log('...ad was blocked, appended message!');
-                } else {
-                    console.log('...ad does not appear blocked!');
-                }
-            }, 1000);
-        });
-    </script>
-	-->
-	<div id="footer_msg" class="panel panel-danger" style="display: none;">
-		<div class="panel-heading"><span class="glyphicon glyphicon-exclamation-sign"></span> I need your help!</div>
-		<div class="panel-body">
-			<h3>If you enjoy this site, please white-list it in your ad-blocking plug-in!</h3>
-			<p class="text-justify">This is a <a href="http://www.assembledrealms.com/user/id/1">one-man</a> operation. I am self-funding with my own hard earned cash and busting my hump trying to make sure everything runs smoothly. A little extra cash from advertising would be awesome. If an ad offended or irritated you, or if you have a suggestion, please feel free to contact me directly: <a href="mailto:chase@assembledrealms.com">chase@assembledrealms.com</a> <a href="https://twitter.com/chasebgale">@chasebgale</a></p>
-		</div>
-	</div>
+</div>
+
+<div id='footer'>
+  <script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
+  <script src='//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  
+  <div class="row">
+    <div id="footer-left" class="col-xs-3">
+      <a href="#">Home</a>
+      ·
+      <a href="#">Account</a>
+      ·
+      <a href="#">Build</a>
+      ·
+      <a href="#">Play</a>
+      ·
+      <a href="#">About</a>
+    </div>
+  
+    <div id="footer-right" class="col-xs-9">
+      <ins id="footer_ad" class="adsbygoogle"
+           style="display:inline-block;width:970px;height:90px;"
+           data-ad-client="ca-pub-4898800745718155"
+           data-ad-slot="4211616849"></ins> 
+      <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+      
+      <script>
+          $(function() {
+              console.log('Document ready!');
+              setTimeout(
+                function() 
+                { 
+                  console.log('1 second later and...');
+                  if ($('#footer_ad').is(':hidden')) {
+                      $('#footer_msg').fadeIn();
+                      console.log('...ad was blocked, appended message!');
+                  } else {
+                      console.log('...ad does not appear blocked!');
+                  }
+              }, 3000);
+          });
+      </script>
+      
+      <div id="footer_msg" style="display: none; width: 970px; height: 90px;">
+        <h3>If you enjoy this site, please white-list it in your ad-blocking plug-in!</h3>
+        <p class="text-justify">This is a <a href="http://www.assembledrealms.com/user/id/1">one-man</a> operation. I am self-funding with my own hard earned cash and busting my hump trying to make sure everything runs smoothly. A little extra cash from advertising would be awesome. If an ad offended or irritated you, or if you have a suggestion, please feel free to contact me directly: <a href="mailto:chase@assembledrealms.com">chase@assembledrealms.com</a> <a href="https://twitter.com/chasebgale">@chasebgale</a></p>
+      </div>
+    </div>
+  
+  </div>
+
 </div>
