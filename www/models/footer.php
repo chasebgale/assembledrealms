0ac669345234269345eee69c98fe2a916304785f
Chase B. Gale <chase.b.gale@gmail.com>
2015-12-25 11:15:49 -0500

    Ad stuff

--- www/models/footer.php
+++ www/models/footer.php
@@ -35,18 +35,18 @@
                 { 
                   console.log('1 second later and...');
                   if ($('#footer_ad').is(':hidden')) {
-                      $('#footer_msg').fadeIn();
+                      $('#footer_msg').fadeIn().css("display","inline-block");
                       console.log('...ad was blocked, appended message!');
                   } else {
                       console.log('...ad does not appear blocked!');
                   }
-              }, 3000);
+              }, 1000);
           });
       </script>
       
       <div id="footer_msg" style="display: none; width: 970px; height: 90px;">
         <h3>If you enjoy this site, please white-list it in your ad-blocking plug-in!</h3>
-        <p class="text-justify">This is a <a href="http://www.assembledrealms.com/user/id/1">one-man</a> operation. I am self-funding with my own hard earned cash and busting my hump trying to make sure everything runs smoothly. A little extra cash from advertising would be awesome. If an ad offended or irritated you, or if you have a suggestion, please feel free to contact me directly: <a href="mailto:chase@assembledrealms.com">chase@assembledrealms.com</a> <a href="https://twitter.com/chasebgale">@chasebgale</a></p>
+        <p>I am self-funding so a little extra cash from advertising would be awesome.</p>
       </div>
     </div>
   
